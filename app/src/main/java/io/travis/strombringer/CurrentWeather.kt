57bc0afcd6e793f306bae94a09ff68acc8e32f64
Travis <TravisEMikolay+GitKraken@Gmail.com>
2022-04-19 18:10:10 -0400

    Add Update to Images Files

--- app/src/main/java/io/travis/strombringer/CurrentWeather.kt
+++ app/src/main/java/io/travis/strombringer/CurrentWeather.kt
@@ -1,4 +1,82 @@
 package io.travis.strombringer
 
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
 class CurrentWeather {
+    private lateinit var name: String // Name = city name
+    private lateinit var icon: String // weather/icon
+    private var dt: Long = 0 // dt
+    private var time: Long = 0 // dt
+    private var temp: Double = 0.0 // main/temp
+    private var humidity: Int = 0
+    private lateinit var description: String //weather/description
+    private lateinit var timezone: String
+
+
+    // private lateinit var humidity: Int // main/humidity
+    // private lateinit var temperature: Int // main/temp
+    //private lateinit var precip: String
+
+    fun getName(): String    { return name }
+    fun setName(name:String) { this.name = name }
+
+    fun getIcon(): String { return icon  }
+    fun setIcon(icon: String) { this.icon = icon }
+
+    fun getIconId(): Int {
+        var iconId:Int = R.drawable.clear_day
+
+        when (icon) {
+            "clear sky" -> iconId = R.drawable.clear_day
+            //"clear sky" -> iconId = R.drawable.clear_night
+            "rain" -> iconId = R.drawable.rain
+            "snow" -> iconId = R.drawable.snow
+            //"sleet" -> iconId = R.drawable.sleet
+            //"wind" -> iconId = R.drawable.wind
+            "mist" -> iconId = R.drawable.fog
+            "scattered clouds" -> iconId = R.drawable.cloudy
+            "few clouds" -> iconId = R.drawable.partly_cloudy
+            //"partly-cloudy-night" -> iconId = R.drawable.cloudy_night
+        }
+
+        return iconId
+    }
+
+
+    fun getDt(): Long { return dt }
+    fun setDt(dt: Long) { this.dt = dt }
+
+    fun getTime(): Long { return time }
+    fun setTime(time: Long) { this.time = time }
+
+    @SuppressLint("SimpleDateFormat")
+    fun getFormattedTime(): String? {
+        val formatter = SimpleDateFormat("h:mm a")
+        formatter.timeZone = TimeZone.getTimeZone(timezone)
+        //val time = dt.toLong();
+        val dateTime = Date(time * 1000)
+        return formatter.format(dateTime)
+    }
+
+    fun getTimeZone():String{ return timezone }
+    fun setTimezone(timezone:String){ this.timezone = timezone }
+
+    fun getTemp(): Double { return temp }
+    fun setTemp(temp:Double) { this.temp = temp }
+
+    fun getHumidity(): Int { return humidity }
+    fun setHumidity(humidity: Int) { this.humidity = humidity }
+
+    fun getDescription(): String { return description }
+    fun setDescription(description: String) { this.description = description }
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/io/travis/strombringer/MainActivity.kt
+++ app/src/main/java/io/travis/strombringer/MainActivity.kt
@@ -7,25 +7,29 @@ import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
 import okhttp3.*
+import org.json.JSONException
+import org.json.JSONObject
 import java.io.IOException
-
 
 class MainActivity : AppCompatActivity() {
 
     private val tag: String = MainActivity::class.java.simpleName
+
+    private var currentWeather = CurrentWeather()
+
+    private val latitude: Double = 37.8267
+    private val longitude: Double = -122.4233
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val apiKey: String = "dc0e0b64666b1ca150b5269a0b7c4ed3"
-        var latitude: Double = 37.8267
-        var longitude: Double = -122.4233
-        var forecastURL: String = "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey";
 
+        val forecastURL: String = "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey"
 
         if (isNetworkAvailable()) {
-            val client = OkHttpClient()
+            val client:OkHttpClient = OkHttpClient()
 
             val request = Request
                 .Builder()
@@ -40,28 +44,33 @@ class MainActivity : AppCompatActivity() {
                 @Throws(IOException::class)
                 override fun onResponse(call: Call, response: Response) {
                     try {
-                        Log.d(tag, response.body!!.string())
+                        val jsonData = response.body!!.string()
+                        Log.d(tag, "JSON Data: $jsonData")
                         if (response.isSuccessful) {
                             Log.d(tag, "isSuccessful")
+                            currentWeather = getCurrentDetails(jsonData)
+                            Log.d(tag,"get Current Time 52 " + currentWeather.getFormattedTime().toString())
                         } else {
                             alertUserAboutError()
                         }
                     } catch (e: IOException) {
                         Log.d(tag, "IO Exception caught: ", e)
+                    } catch (e: JSONException) {
+                        Log.d(tag, "JSON Exception caught: ", e)
                     }
                 }
             })
         }
-
-
-        Log.d(tag,"END")
-
+        else {
+            Toast.makeText(this, "The Network Is Down!",LENGTH_SHORT).show()
+        }
     }
 
+    @Throws(IOException::class)
     private fun isNetworkAvailable(): Boolean {
         val manager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkInfo = manager.activeNetworkInfo
-        var isAvailable = false
+        var isAvailable:Boolean = false
         if (networkInfo != null && networkInfo.isConnected) {
             isAvailable = true
         } else {
@@ -70,9 +79,70 @@ class MainActivity : AppCompatActivity() {
         return isAvailable
     }
 
+    private fun getCurrentDetails(jsonData: String): CurrentWeather {
+        val forecast:JSONObject = JSONObject(jsonData)
+        val weather = forecast.getJSONArray("weather")
+        val main = forecast.getJSONObject("main")
+        val name = forecast.getString("name")
+        val timezone = forecast.getString("timezone")
+        //val dt = forecast.getString("dt")
+        Log.d(tag, "From JSON TIMEZONE!: $timezone")
+
+
+        //val description = weather[0].getString("description")
+
+        val currentWeather = CurrentWeather()
+
+        currentWeather.setName(forecast.getString("name"))
+        currentWeather.setTimezone("timezone")
+        //currentWeather.setIcon(weather.getString("icon"))
+        //currentWeather.setDt(forecast.getLong("dt"))
+        currentWeather.setTime(forecast.getLong("dt"))
+
+        currentWeather.setTemp(main.getDouble("temp"))
+        currentWeather.setHumidity(main.getInt("humidity"))
+        //currentWeather.setDescription(weather.getString("description"))
+
+        Log.d(tag,"From JSON Name: $name")
+        //Log.d(tag,"From JSON Description: $description")
+
+        Log.d(tag,"get Current Time 110 " + currentWeather.getFormattedTime().toString())
+
+//        val description = weather.getString("description")
+//        val icon = weather.getString("icon")
+//        val humidity = main.getString("humidity")
+//
+//        Log.d(tag,"From JSON Description: $description")
+//        Log.d(tag,"From JSON Icon: $icon")
+//        Log.d(tag,"From JSON Name: $humidity")
+
+        return currentWeather
+    }
+
     private fun alertUserAboutError() {
         val dialog = AlertDialogFragment()
         dialog.show(supportFragmentManager, "error_dialog")
-        //dialog.show(FragmentManager, "error_dialog")
     }
+
+//    @Throws(JSONException::class)
+//    private fun getCurrentDetails(jsonData: String): CurrentWeather? {
+//        val forecast = JSONObject(jsonData)
+//        val timezone = forecast.getString("timezone")
+//        Log.i(MainActivity.TAG, "From JSON: $timezone")
+//        val currently = forecast.getJSONObject("currently")
+//        val currentWeather = CurrentWeather()
+//
+//        // Parse weather data from currently object
+//        currentWeather.setHumidity(currently.getDouble("humidity").toInt())
+//        currentWeather.setTime(currently.getLong("time"))
+//        currentWeather.setIcon(currently.getString("icon"))
+//        currentWeather.setLocationLabel("Alcatraz Island, CA")
+//        currentWeather.setPrecipChance(currently.getDouble("precipProbability"))
+//        currentWeather.setSummary(currently.getString("summary"))
+//        currentWeather.setTemperature(currently.getDouble("temperature"))
+//        currentWeather.setTimeZone(timezone)
+//        Log.d(MainActivity.TAG, currentWeather.getFormattedTime()!!)
+//        return currentWeather
+//    }
+
 }
